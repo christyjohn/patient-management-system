344376a107f81917868d91622e6bffd5ba2887a4
Christy John <christyjohn.crz@gmail.com>
2026-09-17 15:48:59 +0530

    setup kafka and added Kafka producer in patient-service

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -1,33 +1,33 @@
-package com.pm.billingservice.grpc;
+    package com.pm.billingservice.grpc;
 
-import billing.BillingRequest;
-import billing.BillingResponse;
-import billing.BillingServiceGrpc.BillingServiceImplBase;
-import io.grpc.stub.StreamObserver;
-import net.devh.boot.grpc.server.service.GrpcService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+    import billing.BillingRequest;
+    import billing.BillingResponse;
+    import billing.BillingServiceGrpc.BillingServiceImplBase;
+    import io.grpc.stub.StreamObserver;
+    import net.devh.boot.grpc.server.service.GrpcService;
+    import org.slf4j.Logger;
+    import org.slf4j.LoggerFactory;
 
-@GrpcService
-public class BillingGrpcService extends BillingServiceImplBase {
+    @GrpcService
+    public class BillingGrpcService extends BillingServiceImplBase {
 
-    private static final Logger log = LoggerFactory.getLogger(
-            BillingGrpcService.class);
+        private static final Logger log = LoggerFactory.getLogger(
+                BillingGrpcService.class);
 
-    @Override
-    public void createBillingAccount(BillingRequest billingRequest,
-                                     StreamObserver<BillingResponse> responseObserver) {
+        @Override
+        public void createBillingAccount(BillingRequest billingRequest,
+                                         StreamObserver<BillingResponse> responseObserver) {
 
-        log.info("createBillingAccount request received {}", billingRequest.toString());
+            log.info("createBillingAccount request received {}", billingRequest.toString());
 
-        // Business logic - e.g save to database, perform calculates etc
+            // Business logic - e.g save to database, perform calculates etc
 
-        BillingResponse response = BillingResponse.newBuilder()
-                .setAccountId("12345")
-                .setStatus("ACTIVE")
-                .build();
+            BillingResponse response = BillingResponse.newBuilder()
+                    .setAccountId("12345")
+                    .setStatus("ACTIVE")
+                    .build();
 
-        responseObserver.onNext(response);
-        responseObserver.onCompleted();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
     }
-}
